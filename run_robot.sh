3063c400979a5b0ddfb9382590b7dc7db37a681f
ztjiawexu <ztjiaweixu@tencent.com>
2024-11-01 19:59:37 +0800

    add runner

--- run_robot.sh
+++ run_robot.sh
@@ -13,6 +13,6 @@ export CUDA_VISIBLE_DEVICES=$cuda_id
 tag=$(date "+%Y%m%d%H%M%S")
 
 python -m legged_gym.scripts.train --task=$task_name --log_root=$log_root --run_name=$run_id --headless \
-    > /logs/${task_name}_${tag}.log 2 > /logs/${task_name}_${tag}.err &
+    > ~/logs/${task_name}_${tag}.out 2> ~/logs/${task_name}_${tag}.err &
 
 # ps -ef | grep legged | awk '{print $2}' | xargs kill -9
